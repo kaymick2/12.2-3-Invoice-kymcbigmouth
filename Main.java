6cc1e2e16f4cead7d0488ff02305acb462f3ca76
Kyle McDowell <kymcd21@icstudents.org>
2021-03-04 08:51:38 +0000

    yup

--- Main.java
+++ Main.java
@@ -7,13 +7,14 @@ public class Main
    public static void main(String[] args)
    {  
       Address samsAddress 
-            = new Address("JONES BBQ AND FOOT",
-               "100 Main Street", "Anytown", "CA", "98765");
+            = new Address("Appliance Direct",
+               "488 FL-436 Suit 1340", "Altamonte Springs", "FL", "32714");
 
       Invoice samsInvoice = new Invoice(samsAddress);
-      samsInvoice.add(new Product("Toaster", 29.95), 3);
-      samsInvoice.add(new Product("Hair dryer", 24.95), 1);
-      samsInvoice.add(new Product("Car vacuum", 19.99), 2);
+      samsInvoice.add(new Product("Whirlpool 33-inch Side-By-Side Refrigerator, Model WRS321SDHZ", 1397.77), 1);
+      samsInvoice.add(new Product("Whirlpool 1.9 cu-ft Microwave, Model WMH32519HZ", 297.77), 1);
+      samsInvoice.add(new Product("Toaster + Knife set", 19.99), 1);
+      samsInvoice.add(new Product("Square D 20 Amp Single-Pole Plug-On Neutral Dual Function Circuit Breaker", 48), 1);
 
       System.out.println(samsInvoice.format());           
    }
